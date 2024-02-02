4fd917f6c0a3379282325882092e77e2a2435af4
bruno <bruno.costa1203@yahoo.com>
2024-02-02 12:10:15 -0300

    Projeto api de receitas finalizado

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -12,12 +12,12 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("recipe")]
 public class RecipesController : ControllerBase
-{    
+{
     public readonly IRecipeService _service;
-    
+
     public RecipesController(IRecipeService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 1 - Sua aplicação deve ter o endpoint GET /recipe
@@ -25,35 +25,65 @@ public class RecipesController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        throw new NotImplementedException();    
+        var recipes = _service.GetRecipes();
+        return Ok(recipes);
     }
 
     // 2 - Sua aplicação deve ter o endpoint GET /recipe/:name
     //Read
     [HttpGet("{name}", Name = "GetRecipe")]
     public IActionResult Get(string name)
-    {                
-        throw new NotImplementedException();
+    {
+        var recipes = _service.GetRecipe(name);
+        return Ok(recipes);
     }
 
     // 3 - Sua aplicação deve ter o endpoint POST /recipe
     [HttpPost]
-    public IActionResult Create([FromBody]Recipe recipe)
+    public IActionResult Create([FromBody] Recipe recipe)
     {
-        throw new NotImplementedException();
+        if (recipe == null)
+        {
+            return BadRequest();
+        }
+        _service.AddRecipe(recipe);
+        return CreatedAtRoute("GetRecipe", new { name = recipe.Name }, recipe);
     }
 
     // 4 - Sua aplicação deve ter o endpoint PUT /recipe
     [HttpPut("{name}")]
-    public IActionResult Update(string name, [FromBody]Recipe recipe)
+    public IActionResult Update(string name, [FromBody] Recipe recipe)
     {
-        throw new NotImplementedException();
+        if (recipe == null)
+        {
+            return BadRequest();
+        }
+        var existingRecipe = _service.RecipeExists(name);
+        if (existingRecipe == false)
+        {
+            return BadRequest();
+        }
+        try
+        {
+            _service.UpdateRecipe(recipe);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
     }
 
     // 5 - Sua aplicação deve ter o endpoint DEL /recipe
     [HttpDelete("{name}")]
     public IActionResult Delete(string name)
     {
-        throw new NotImplementedException();
-    }    
+        var existingRecipe = _service.RecipeExists(name);
+        if (existingRecipe == false)
+        {
+            return NotFound();
+        }
+        _service.DeleteRecipe(name);
+        return NoContent();
+    }
 }
--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -12,39 +12,67 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("user")]
 public class UserController : ControllerBase
-{    
+{
     public readonly IUserService _service;
-    
+
     public UserController(IUserService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 6 - Sua aplicação deve ter o endpoint GET /user/:email
     [HttpGet("{email}", Name = "GetUser")]
     public IActionResult Get(string email)
-    {                
-        throw new NotImplementedException();
+    {
+        var user = _service.GetUser(email);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
     }
 
     // 7 - Sua aplicação deve ter o endpoint POST /user
     [HttpPost]
-    public IActionResult Create([FromBody]User user)
+    public IActionResult Create([FromBody] User user)
     {
-        throw new NotImplementedException();
+        if (user == null)
+        {
+            return BadRequest();
+        }
+        _service.AddUser(user);
+        return CreatedAtRoute("GetUser", new { email = user.Email }, user);
     }
 
     // "8 - Sua aplicação deve ter o endpoint PUT /user
     [HttpPut("{email}")]
-    public IActionResult Update(string email, [FromBody]User user)
+    public IActionResult Update(string email, [FromBody] User user)
     {
-        throw new NotImplementedException();
+        if (_service.UserExists(email))
+        {
+            return NotFound();
+        }
+        try
+        {
+            _service.UpdateUser(user);
+            return Ok();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
     }
 
     // 9 - Sua aplicação deve ter o endpoint DEL /user
     [HttpDelete("{email}")]
     public IActionResult Delete(string email)
     {
-        throw new NotImplementedException();
-    } 
+        var existingUser = _service.UserExists(email);
+        if (existingUser == false)
+        {
+            return NotFound();
+        }
+        _service.DeleteUser(email);
+        return NoContent();
+    }
 }
